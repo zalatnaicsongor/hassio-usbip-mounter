fc362543e05bf3179de447bb577a15d4cf3d607d
Csongor Zalatnai <csongor@zalatnai.hu>
2024-03-25 23:13:55 +0100

    Update create_devices.sh

--- usbip/rootfs/etc/cont-init.d/create_devices.sh
+++ usbip/rootfs/etc/cont-init.d/create_devices.sh
@@ -28,6 +28,8 @@ if ! bashio::fs.file_exists "${mount_script}"; then
   echo '#!/command/with-contenv bashio' > "${mount_script}"
   echo 'set -x' >> "${mount_script}"
   echo 'mount -o remount -t sysfs sysfs /sys' >> "${mount_script}"
+  echo '/usr/sbin/usbip --debug port' >> "${mount_script}"
+  echo 'for p in $(/usr/sbin/usbip port | grep Port | cut -b 6-7); do /usr/sbin/usbip --debug detach -p "$p"; done' >> "${mount_script}"
   for device in $(bashio::config 'devices|keys'); do
     server_address=$(bashio::config "devices[${device}].server_address")
     bus_id=$(bashio::config "devices[${device}].bus_id")
